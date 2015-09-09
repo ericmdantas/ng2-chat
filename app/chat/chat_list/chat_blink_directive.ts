ff42c06f773402cd3371c80f156bc7819b847518
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-09-09 07:31:05 -0300

    .;.;

--- app/chat/chat_list/chat_blink_directive.ts
+++ app/chat/chat_list/chat_blink_directive.ts
@@ -1,19 +1,37 @@
-/// <reference path="../../typings/tsd.d.ts" />
+/// <reference path="../../../typings/tsd.d.ts" />
 
 import {Directive, ElementRef, LifecycleEvent, EventEmitter} from 'angular2/angular2';
 import {Inject} from 'angular2/di';
-import {ChatService} from 'app/chat/services/chat_service.js';
+
+type dirType = {
+  fontColor: string;
+  bkg: string;
+}
 
 @Directive({
-  selector: '[blink-message]'
+  selector: '[blink-message]',
+  lifecycle: [LifecycleEvent.onAllChangesDone]
 })
 
 export class ChatBlinkDirective {
-  constructor(@Inject(ChatService) private _chatService: ChatService) {
+  static DEFAULT_COLORS: dirType = {bkg: 'transparent', fontColor: "#444"};
+  static SHINY_COLORS: dirType = {bkg: 'orange', fontColor: "#fff"};
+
+  constructor(@Inject(ElementRef) private _el: ElementRef) {
 
   }
 
-  onInit() {
+  onAllChangesDone() {
+    this._blink();
+  }
 
+  private _blink():void {
+    this._el.nativeElement.style.backgroundColor = ChatBlinkDirective.DEFAULT_COLORS.bkg;
+    this._el.nativeElement.style.color = ChatBlinkDirective.DEFAULT_COLORS.fontColor;
+
+    setTimeout(() => {
+      this._el.nativeElement.style.backgroundColor = ChatBlinkDirective.SHINY_COLORS.bkg;
+      this._el.nativeElement.style.color = ChatBlinkDirective.SHINY_COLORS.fontColor;
+    }, 999);
   }
 }
