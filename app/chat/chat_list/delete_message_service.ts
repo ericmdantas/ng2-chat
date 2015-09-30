1d9d9ec4ecbf7417565da980b4902d862013faeb
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-09-30 15:53:07 -0300

    .;.;.;.;.

--- app/chat/chat_list/delete_message_service.ts
+++ app/chat/chat_list/delete_message_service.ts
@@ -3,7 +3,6 @@ import {MessageModel} from 'app/chat/model/message_model.js';
 export class DeleteMessageService {
   remove(message:MessageModel) {
     setTimeout(() => {
-      debugger;
       message.deleted = true;
       message.typing = false;
     }, message.deleteTime);
