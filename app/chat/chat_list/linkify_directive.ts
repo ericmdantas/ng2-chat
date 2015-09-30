350f0d9c119951d78b296f611c980df21ee4b755
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-09-29 23:40:32 -0300

    .;.;.;.

---
+++ app/chat/chat_list/linkify_directive.ts
@@ -0,0 +1,25 @@
+/// <reference path="../../typings/tsd.d.ts" />
+
+import {Directive, Inject, ElementRef} from 'angular2/angular2';
+import {MessageModel} from 'app/chat/model/message_model.js';
+
+@Directive({
+  selector: 'linkify'
+})
+export class LinkifyDirective {
+  private static LINK_PATTERN: RegExp = /^https?/g;
+
+  constructor(@Inject(ElementRef) private _elementRef: ElementRef) {
+
+  }
+
+  public parseLink(m: MessageModel):void {
+    this._parseLink(m);
+  }
+
+  private _parseLink(m: MessageModel):void {
+    if (LinkifyDirective.LINK_PATTERN.test(m.message)) {
+      console.log('link');
+    }
+  }
+}
